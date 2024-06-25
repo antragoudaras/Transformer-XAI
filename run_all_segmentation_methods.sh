8a071eaaaddd9c0e5452f2e28b3fbfe7595ec26a
antragoudaras <antonistragoudaras@gmail.com>
2024-06-25 22:27:02 +0200

    update job scripts

---
+++ run_all_segmentation_methods.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+methods=("rollout" "full_lrp" "transformer_attribution" "lrp_last_layer" "attn_last_layer" "attn_gradcam" "full_lrp")
+
+for method in "${methods[@]}"
+do
+    echo "Running method: $method"
+    sbatch --job-name "${method}_" segmentation_initial_methods.job --method "${method}" --imagenet-seg-path "./gtsegs_ijcv.mat" 
+done
