e7d30deed125d078690f7e184f791fa4e30cd71b
antragoudaras <antonistragoudaras@gmail.com>
2024-06-27 01:48:23 +0200

    adding DEiT results

--- run_all_segmentation_methods_deit.sh
+++ run_all_segmentation_methods_deit.sh
@@ -11,5 +11,5 @@ methods=("rollout" "gradient_rollout_cls_spec" "transformer_attribution" "lrp_la
 for method in "${methods[@]}"
 do
     echo "Running method: $method"
-    sbatch --job-name "${method}" segmentation_initial_methods_deit.job --method "${method}" --imagenet-seg-path "./gtsegs_ijcv.mat" 
+    sbatch --job-name "${method}_deit" segmentation_initial_methods_deit.job --method "${method}" --imagenet-seg-path "./gtsegs_ijcv.mat" 
 done
