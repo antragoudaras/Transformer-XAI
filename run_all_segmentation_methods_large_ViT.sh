0d1a5ff4589c1f656f28dc8ffd2bfb2bc65fd0ac
antragoudaras <antonistragoudaras@gmail.com>
2024-06-28 02:24:39 +0200

    adding scripts and logs for ViT-L

---
+++ run_all_segmentation_methods_large_ViT.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+methods=("rollout" "gradient_rollout_cls_spec" "transformer_attribution" "lrp_last_layer" "attn_last_layer" "attn_gradcam" "full_lrp")
+# methods=("attn_gradcam")
+# methods=("gradient_rollout_cls_spec")
+# methods=("rollout")
+
+for method in "${methods[@]}"
+do
+    echo "Running method: $method"
+    sbatch --job-name "${method}_large_ViT" segmentation_initial_methods_large_ViT.job --method "${method}" --imagenet-seg-path "./gtsegs_ijcv.mat" 
+done
